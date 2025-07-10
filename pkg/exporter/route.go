57e18fdd661986870c8aac91b62488d657bf6365
Nick Jüttner <nick@giantswarm.io>
2025-07-10 18:57:48 +0200

    feat: Add info log when forwarding events to a receiver

--- pkg/exporter/route.go
+++ pkg/exporter/route.go
@@ -1,6 +1,10 @@
 package exporter
 
-import "github.com/giantswarm/kubernetes-event-exporter/pkg/kube"
+import (
+	"github.com/rs/zerolog/log"
+
+	"github.com/giantswarm/kubernetes-event-exporter/pkg/kube"
+)
 
 // Route allows using rules to drop events or match events to specific receivers.
 // It also allows using routes recursively for complex route building to fit
@@ -24,6 +28,13 @@ func (r *Route) ProcessEvent(ev *kube.EnhancedEvent, registry ReceiverRegistry) 
 	for _, rule := range r.Match {
 		if rule.MatchesEvent(ev) {
 			if rule.Receiver != "" {
+				log.Info().
+					Str("receiver", rule.Receiver).
+					Str("kind", ev.InvolvedObject.Kind).
+					Str("name", ev.InvolvedObject.Name).
+					Str("namespace", ev.Namespace).
+					Str("reason", ev.Reason).
+					Msg("Forwarding event to receiver")
 				registry.SendEvent(rule.Receiver, ev)
 				// Send the event down the hole
 			}
